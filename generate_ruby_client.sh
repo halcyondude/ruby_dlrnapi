a3afec2ad812ef44339de47b4fb6361743631635
Matt Young <myoung@redhat.com>
2017-10-13 01:38:36 -0400

    Add swagger config files for api generation

--- generate_ruby_client.sh
+++ generate_ruby_client.sh
@@ -2,9 +2,15 @@
 
 wget -O /tmp/swagger-codegen-cli-2.2.3.jar https://oss.sonatype.org/content/repositories/releases/io/swagger/swagger-codegen-cli/2.2.3/swagger-codegen-cli-2.2.3.jar
 
+# generate the ruby client
 java -jar /tmp/swagger-codegen-cli-2.2.3.jar generate \
     -i https://raw.githubusercontent.com/softwarefactory-project/DLRN/master/doc/api_definition.yaml \
     -l ruby \
-    -c swaggerconfig.json \
-    -l dynamic-html
-    
+    -c swaggerconfig-ruby.json \
+
+# generate the static heml documentation
+java -jar /tmp/swagger-codegen-cli-2.2.3.jar generate \
+    -i https://raw.githubusercontent.com/softwarefactory-project/DLRN/master/doc/api_definition.yaml \
+    -l html \
+    -c swaggerconfig-html.json \
+
